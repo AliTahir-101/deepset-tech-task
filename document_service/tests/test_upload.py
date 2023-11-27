440f10958791c5d15fdb5f4780eb4efd1ff9470d
Ali Tahir <alitahir231@gmail.com>
2023-11-27 10:13:30 +0200

    Update to add test cases around upload endpoint & Code Cleanup 🥰

---
+++ document_service/tests/test_upload.py
@@ -0,0 +1,30 @@
+import pytest
+
+from fastapi.testclient import TestClient
+from src.main import app
+
+
+class TestUploadEndpoint:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        # Setup code here. Will run before each test method.
+        # Can setup a test client or mock services.
+        self.client = TestClient(app)
+
+    def test_upload_file_success(self):
+        # Simulate a file upload
+        response = self.client.post(
+            "/upload/",
+            files={"file": ("filename.txt", "file content")}
+        )
+        assert response.status_code == 200
+        assert "file_id" in response.json()
+        assert response.json()[
+            "status"] == "File uploaded and processing queued"
+
+    def test_upload_file_failure(self):
+        # Simulate a file upload failure, perhaps by not sending a file
+        response = self.client.post("/upload/")
+        assert response.status_code == 422  # Assuming 422 for invalid input
+
+    # Could add more tests for different scenarios
